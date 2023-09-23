aecd5b09d9481900d4f3b183f76e7c0c006d4f14
Adriana Martins <drica317@gmail.com>
2023-09-23 19:49:07 -0300

    adaptando para o meu blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,16 +1,79 @@
 export const datafake = [
     {
         "id":"1",
-        "title": "NOVO HOMEM DE FERRO EM 3D",
-        "description": "Marvel Studios anuncia novo filme do homem de ferro",
-        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg"
+        "title": "ME CHAME PELO SEU NOME",
+        "description": "Conta a história de amor e descobertas de um adolescente de 17 anos por um homem mais velho, de 24 anos, durante uma viagem de férias",
+        "photoCover": "https://cdn.culturagenial.com/imagens/me-chame-pelo-seu-nome-0-cke.jpg"
     },
     {
         "id":"2",
-        "title": "Nova série anunciada na Dysney",
-        "description": "Ola",
-        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+        "title": "P.S. EU TE AMO",
+        "description": "Conta sobre Holly, uma mulher de 30 anos que está lutando para superar a perda de seu grande amor, Gerry.",
+        "photoCover": "https://cdn.culturagenial.com/imagens/ps-eu-te-amo-livro-cke.jpg"
+    },
+    {
+        "id":"3",
+        "title": "ANNE DE GREEN GABLES",
+        "description": "O livro se tornou um clássico comparado à Pollyanna, de Eleanor H. Porter, por trazer a figura de uma garota órfã que consegue enxergar as belezas da vida, mesmo com várias adversidades.",
+        "photoCover": "https://cdn.culturagenial.com/imagens/anne-de-green-gables-livro-cke.jpg"
+    },
+    {
+        "id":"4",
+        "title": "AGUA PARA ELEFANTES",
+        "description": "Conta sobre Jacob Jankowski, um homem já idoso que relata suas memórias e vivências em um circo itinerante.",
+        "photoCover": "https://cdn.culturagenial.com/imagens/gua-para-elefantes-livro-cke.jpg"
+    },
+
+    {
+        "id":"5",
+        "title": "LIVROS DE DRAMA",
+        "description": "Uma das maneiras de se envolver com a leitura, é escolher um gênero literário que prenda sua atenção e drama é um dos gêneros mais populares desde a grécia antiga.",
+        "photoCover": "https://thumbs.dreamstime.com/b/m%C3%A1scaras-do-drama-38191460.jpg"
+    },
+    {
+        "id":"6",
+        "title": "O SOL É PARA TODOS",
+        "description": "Nesta emocionante história ambientada no Sul dos Estados Unidos da década de 1930, região envenenada pela violência do preconceito racial, vemos um mundo de grande beleza e ferozes desigualdades através dos olhos de uma menina de inteligência viva e questionadora.",
+        "photoCover": "https://ohoje.com/public/imagens/fotos/amp/2023/05/51wdOrz6uNL._SY344_BO1204203200_QL70_ML2_.jpg"
+    },
+    {
+        "id":"7",
+        "title": "AS VINHAS DA IRA",
+        "description": "As vinhas da ira representa o confronto entre indivíduo e sociedade, através da epopeia da família Joad, expulsa pela seca dos campos de algodão de Oklahoma, para tentar a sobrevivência como boias-frias nas plantações de frutas do Vale de Salinas, na Califórnia",
+        "photoCover": "https://ohoje.com/public/imagens/fotos/amp/2023/05/91SOU6MzlEL-673x1024.jpg"
+    },
+    {
+        "id":"8",
+        "title": "O AMOR NOS TEMPOS DO CÓLERA",
+        "description": "Essa é a história real dos pais de Gabriel García Márquez e foi ponto de partida de O amor nos tempos do cólera, que acompanha a paixão do telegrafista, violinista e poeta Florentino Ariza por Fermina Daza.",
+        "photoCover": "https://ohoje.com/public/imagens/fotos/amp/2023/05/41mruiKEWnL._SX343_BO1204203200_.jpg"
+    },
+    {
+        "id":"9",
+        "title": "LIVROS INFANTIS",
+        "description": "Para além da importância didática, a leitura na infância é fundamental para despertar o interesse pela literatura e fazer com que se experimente sentimentos complexos que farão parte da vida adulta.",
+        "photoCover": "https://img.freepik.com/vetores-premium/educacao-infantil_29937-3076.jpg"
+    },
+    {
+        "id":"10",
+        "title": "O PEQUENO PRÍNCIPE",
+        "description": "Esta obra é marcada pelo seu alto teor filosófico e poético, mesmo sendo considerada a princípio uma literatura para crianças.",
+        "photoCover": "https://cdn.culturagenial.com/imagens/s-l1000-cke.jpg"
+    },
+    {
+        "id":"11",
+        "title": "PLUFT O FANTASMINHA",
+        "description": "A narrativa acompanha a amizade surgida entre Maribel, uma jovem garota, e Pluft, um fantasma que vive em uma velha casa e tem muito medo de gente.",
+        "photoCover": "https://cdn.culturagenial.com/imagens/pluft-o-fantasminha.jpg"
+    },
+    {
+        "id":"12",
+        "title": "O MENINO MALUQUINHO",
+        "description": "O Menino Maluquinho apresenta um garoto travesso, criativo e cheio de energia. Escrito e ilustrado por Ziraldo nos anos oitenta, o livro, em formato de quadrinhos, foi adaptado mais tarde para os mais diversos meios (TV, teatro, cinema).",
+        "photoCover": "https://cdn.culturagenial.com/imagens/978-85-06-05510-6-menino-maluquinho-768x1141-0-cke.jpg"
     }
+
+
 ]
     
   
